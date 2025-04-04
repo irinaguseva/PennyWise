ff342802c1aeb009e21ef79d7aeaaa40c8eda9fd
irinaguseva <ireneguseva9@gmail.com>
2025-04-04 22:37:48 +0300

    refactoring urls

--- budget/urls.py
+++ budget/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import BalanceView, CategoryTotalView, CategoryViewSet, TransactionViewSet
+from .views import BalanceView, CategoryTotalView, CategoryViewSet, TransactionViewSet, CategoryReportView
 
 router = DefaultRouter()
 router.register(r"categories", CategoryViewSet, basename="category")
@@ -10,7 +10,6 @@ router.register(r"transactions", TransactionViewSet, basename="transaction")
 urlpatterns = [
     path("", include(router.urls)),
     path("balance/", BalanceView.as_view(), name="balance"),
-    path(
-        "total/<str:category>/", CategoryTotalView.as_view(), name="specific-category"
-    ),
+    path("total/<str:category>/", CategoryTotalView.as_view(), name="specific-category"),
+    path("report/", CategoryReportView.as_view(), name="specific-category"),
 ]
