2fa0f26a8cc0bc3b648d3e251c6ed4ad794f62dd
irinaguseva <ireneguseva9@gmail.com>
2025-04-06 23:36:16 +0300

    added minor fixes to settings

--- pennywise/settings.py
+++ pennywise/settings.py
@@ -57,8 +57,11 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=2400),  # Увеличьте если нужно
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
 MIDDLEWARE = [
