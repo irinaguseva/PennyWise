8b96973af1bfe90833d84aa1412f741d49ece944
irinaguseva <ireneguseva9@gmail.com>
2025-04-26 17:02:48 +0300

    moved balance route to transactions

--- budget/urls.py
+++ budget/urls.py
@@ -1,13 +1,10 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import BalanceView
-
 router = DefaultRouter()
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("balance/", BalanceView.as_view(), name="balance"),
     path("categories/", include("categories.urls")),
     path("transactions/", include("transactions.urls")),
 ]
--- transactions/urls.py
+++ transactions/urls.py
@@ -1,7 +1,8 @@
-from django.urls import include, path
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from budget.views import (
+    BalanceView,
     CategoryReportView,
     FinancialRecommendationView,
     TransactionViewSet,
@@ -11,10 +12,7 @@ router = DefaultRouter()
 router.register(r"", TransactionViewSet, basename="transaction")
 
 urlpatterns = [
-    path(
-        "recommendation/",
-        FinancialRecommendationView.as_view(),
-        name="financial-recommendation",
-    ),
+    path("recommendation/", FinancialRecommendationView.as_view(), name="financial-recommendation"),
     path("report/", CategoryReportView.as_view(), name="specific-category"),
+    path("balance/", BalanceView.as_view(), name="balance"),
 ] + router.urls
