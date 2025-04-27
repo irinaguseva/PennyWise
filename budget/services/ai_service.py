9bb5ebd550adfd3a48b57de1726281e416900504
irinaguseva <ireneguseva9@gmail.com>
2025-04-27 16:18:30 +0300

    move ai prompt to a separate module

--- budget/services/ai_service.py
+++ budget/services/ai_service.py
@@ -12,30 +12,11 @@ class AIService:
     def __init__(self):
         self.token = 'N2E0N2NlMDAtMTZjYy00ZGI2LTllYTUtYzQwNTVkNjA0ZjRkOjdiN2I4ZmIzLTc4MWQtNGVlYS1iZjBhLTA3ZjNjMWYyMmI4YQ=='
 
-    def build_prompt(self, financial_data, question):
-        return f"""
-                Ты финансовый помощник. Пользователь спрашивает: "{question}".
-
-                Вот финансовая информация пользователя:
-                - Текущий баланс: {financial_data['balance']} руб.
-                - Доходы за период: {financial_data['income']} руб.
-                - Расходы за период: {financial_data['expenses']} руб.
-
-                Основные категории расходов:
-                {', '.join([f"{cat['category']} ({cat['percentage']:.1f}%)" for cat in financial_data['category_expenses']])}
-
-                Дай конкретные рекомендации на русском языке, основанные на этих данных. 
-                Будь дружелюбным и профессиональным. Если нужно сократить расходы, предложи конкретные категории.
-                
-                Напоминаю, вот вопрос: {question}
-                """
-
     def get_recommendation(self, financial_data, question):
         giga = GigaChat(
             credentials=self.token,
             verify_ssl_certs=False,
         )
-        #prompt = self.build_prompt(financial_data, question)
         prompt = AIPromptBuilder.build_financial_advise_prompt(financial_data, question)
 
         messages = [SystemMessage(content=prompt)]
