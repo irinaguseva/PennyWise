d814d7e313dfc4b5d08efdb89b5431b27dc13ea7
irinaguseva <ireneguseva9@gmail.com>
2025-05-05 21:54:08 +0300

    added report excel fileresponse view

---
+++ budget/views/report_download.py
@@ -0,0 +1,25 @@
+from django.http import FileResponse
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+
+from budget.utils.excel_report_generator import generate_test_excel
+from budget.utils.get_data_for_report import get_user_data_for_report
+
+
+class ReportDownloadView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+
+        start_date_str = request.query_params.get("start_date")
+        end_date_str = request.query_params.get("end_date")
+
+        report_data = get_user_data_for_report(start_date_str, end_date_str, request.user)
+
+        file_path = generate_test_excel(report_data)
+
+        response = FileResponse(
+        open(file_path, 'rb'),
+        content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        )
+        response['Content-Disposition'] = f'attachment; filename="IRA_TEST1.xlsx"'
+        return response
