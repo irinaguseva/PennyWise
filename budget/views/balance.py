03c6b4e44c8fb310c36e3e283e52a5670f5fd9b4
irinaguseva <ireneguseva9@gmail.com>
2025-05-31 22:15:01 +0300

    added an opportunity to get total balance data for particular category or no category at all

--- budget/views/balance.py
+++ budget/views/balance.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .tools.get_financial_data import get_total_by_type
+from categories.models import Category
 
 
 class BalanceView(APIView):
@@ -11,18 +12,25 @@ class BalanceView(APIView):
 
     def get(self, request):
         user = request.user
+        category_name = request.query_params.get("category")
 
         total_income = get_total_by_type(user=user, transaction_type="income")
         total_expense = get_total_by_type(user=user, transaction_type="expense")
-
         balance = total_income - total_expense
 
-        return Response(
-            {
+        response_body = {
                 "total_income": total_income,
                 "total_expense": total_expense,
                 "balance": balance,
+            }
+        if category_name:
+            category = Category.objects.get(name=category_name)
+            total_expense_by_cat = get_total_by_type(user=user, transaction_type="expense", category=category)
+            response_body[f"total_expense_in_category_{category_name}"] = total_expense_by_cat
+            total_income_by_cat = get_total_by_type(user=user, transaction_type="income", category=category)
+            response_body[f"total_income_in_category_{category_name}"] = total_income_by_cat
 
-            },
+        return Response(
+            response_body,
             status=status.HTTP_200_OK,
         )
